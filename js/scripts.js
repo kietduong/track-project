62d21834cd39a0dc173a138348968aa9098005a8
Kiet Duong <kd14g@my.fsu.edu>
2016-08-24 21:05:20 -0700

    resets scripts to add more content to quiz results

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,48 @@
-$(document).ready(function() {
-  $("form#track").submit(function(event) {
-    var kind = $("select#end").val();
-    $("#button").click(function() {
-       $("#css").hide();
-       $("#java").hide();
-       $("#php").hide();
-    });
+//back end
+var css = 0
+var php = 0
+var jva = 0
 
-    if(kind) {
-      if(kind === "front") {
-         $("#css").show();
-      }
-      else if(kind === "back") {
-         $("#php").show();
-      }
-      else {
-         $("#java").show();
-      }
+
+
+
+//frnt end
+$(document).ready(function() {
+  $("#button").click(function() {
+
+    var end = $("#end").val();
+      if (end === "front") {
+      css += 1;
+    }else if (end === "back") {
+      php += 1;
+    }else {
+      jva +=1;
+    }
+    var preference = $("#surveyquestion2").val();
+      if (preference === "design1") {
+      css += 1;
+    }else {
+      jva +=1;
+    }
+    var career = $("#surveyquestion3").val();
+      if (career === "design2") {
+      css += 1;
+    }else if (end === "development2") {
+      jva += 1;
+    }else {
+      php +=1;
+    }
+    if (jva > php && jva > css) {
+    $("#java").show();
+    }
+    if (php > jva && php > css) {
+    $("#php").show();
+    }
+    if (css > php && jva < css) {
+    $("#css").show();
     }
 
-  event.preventDefault();
+
 
   });
 
